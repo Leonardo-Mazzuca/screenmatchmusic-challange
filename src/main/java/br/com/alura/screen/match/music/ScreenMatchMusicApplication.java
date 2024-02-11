435ba05820898bb5934b2ac64e2d695280637d74
Leonardo-Mazzuca <leonardodesouzamazzuca8@gmail.com>
2024-02-11 12:12:13 -0300

    first commit

---
+++ src/main/java/br/com/alura/screen/match/music/ScreenMatchMusicApplication.java
@@ -0,0 +1,15 @@
+package br.com.alura.screen.match.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchMusicApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ScreenMatchMusicApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/br/com/alura/screen/match/music/ScreenMatchMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screen.match.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
